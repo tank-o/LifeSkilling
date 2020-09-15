064cc3ed23c3b223f22dc442b7079daf0a1dd305
tank-o <61785671+tank-o@users.noreply.github.com>
2020-09-15 10:29:16 +0100

    Finished Lumbering, now just need to clean up some code

--- src/main/java/me/tanko_/lifeskills/BlockBreak/BlockBreak.java
+++ src/main/java/me/tanko_/lifeskills/BlockBreak/BlockBreak.java
@@ -2,7 +2,6 @@ package me.tanko_.lifeskills.BlockBreak;
 
 import me.tanko_.lifeskills.CustomItems.CustomTools;
 import me.tanko_.lifeskills.Lumbering.Lumbering;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -10,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,44 +16,39 @@ import java.util.Collections;
 public class BlockBreak implements Listener {
 
     @EventHandler
-    public void BlockBreak(BlockBreakEvent e){
+    public void BlockBreak(BlockBreakEvent e) {
         Player player = e.getPlayer();
         Block block = e.getBlock();
         //Axes
-        ArrayList<ItemMeta> Axes = new ArrayList<ItemMeta>();
-        Collections.addAll(Axes,CustomTools.ApprenticeAxe().getItemMeta(),CustomTools.OneApprenticeAxe().getItemMeta(),CustomTools.TwoApprenticeAxe().getItemMeta(),
-                CustomTools.ThreeApprenticeAxe().getItemMeta(),CustomTools.FourApprenticeAxe().getItemMeta(),CustomTools.FiveApprenticeAxe().getItemMeta(),
-                CustomTools.MasterAxe().getItemMeta(),CustomTools.OneMasterAxe().getItemMeta(),CustomTools.TwoMasterAxe().getItemMeta(),CustomTools.ThreeMasterAxe().getItemMeta(),
-                CustomTools.FourMasterAxe().getItemMeta(),CustomTools.FiveMasterAxe().getItemMeta());
-        if (!player.getInventory().getItemInMainHand().equals(Material.AIR)) {
-            ItemMeta toolMeta = player.getInventory().getItemInMainHand().getItemMeta();
-            for (int i = 0; i < Axes.size(); i++) {
-                if (toolMeta.equals(Axes.get(i))) {
-                    Location Block = e.getBlock().getLocation();
-                    if (block.getType().equals(Material.ACACIA_LOG)) {
-                        Lumbering.GetDrops(player, "acacia", Block);
-                    } else if (block.getType().equals(Material.BIRCH_LOG)) {
-                        player.sendMessage("you broke a birch wood");
-                        Lumbering.GetDrops(player, "birch", Block);
-                    } else if (block.getType().equals(Material.SPRUCE_LOG)) {
-                        Lumbering.GetDrops(player, "spruce", Block);
-                    } else if (block.getType().equals(Material.OAK_LOG)) {
-                        Lumbering.GetDrops(player, "oak", Block);
-                    } else if (block.getType().equals(Material.JUNGLE_LOG)) {
-                        Lumbering.GetDrops(player, "jungle", Block);
-                    } else if (block.getType().equals(Material.DARK_OAK_LOG)) {
-                        Lumbering.GetDrops(player, "darkoak", Block);
-                    }
-                    e.getBlock().setType(Material.AIR);
-                } else {
-                    e.setCancelled(true);
+        ArrayList<String> Axes = new ArrayList<>();
+        Collections.addAll(Axes, CustomTools.ApprenticeAxe().getItemMeta().getDisplayName(), CustomTools.OneApprenticeAxe().getItemMeta().getDisplayName(), CustomTools.TwoApprenticeAxe().getItemMeta().getDisplayName(),
+                CustomTools.ThreeApprenticeAxe().getItemMeta().getDisplayName(), CustomTools.FourApprenticeAxe().getItemMeta().getDisplayName(), CustomTools.FiveApprenticeAxe().getItemMeta().getDisplayName(),
+                CustomTools.MasterAxe().getItemMeta().getDisplayName(), CustomTools.OneMasterAxe().getItemMeta().getDisplayName(), CustomTools.TwoMasterAxe().getItemMeta().getDisplayName(), CustomTools.ThreeMasterAxe().getItemMeta().getDisplayName(),
+                CustomTools.FourMasterAxe().getItemMeta().getDisplayName(), CustomTools.FiveMasterAxe().getItemMeta().getDisplayName());
+        int heldSlot = player.getInventory().getHeldItemSlot();
+        ItemStack tool = player.getInventory().getItem(heldSlot);
+        for (int i = 0; i < Axes.size();i++) {
+            if (tool.getItemMeta().getDisplayName().equals(Axes.get(i))) {
+                if (block.getType().equals(Material.ACACIA_LOG)){
                     e.setDropItems(false);
-                    break;
+                    Lumbering.GetDrops(player,"acacia",block.getLocation());
+                } else if (block.getType().equals(Material.BIRCH_LOG)){
+                    e.setDropItems(false);
+                    Lumbering.GetDrops(player,"birch",block.getLocation());
+                } else if (block.getType().equals(Material.OAK_LOG)){
+                    e.setDropItems(false);
+                    Lumbering.GetDrops(player,"oak",block.getLocation());
+                } else if (block.getType().equals(Material.SPRUCE_LOG)){
+                    e.setDropItems(false);
+                    Lumbering.GetDrops(player,"spruce",block.getLocation());
+                } else if (block.getType().equals(Material.JUNGLE_LOG)){
+                    e.setDropItems(false);
+                    Lumbering.GetDrops(player,"jungle",block.getLocation());
+                } else if (block.getType().equals(Material.DARK_OAK_LOG)){
+                    e.setDropItems(false);
+                    Lumbering.GetDrops(player,"darkoak",block.getLocation());
                 }
             }
-        }else {
-            e.setCancelled(true);
-            e.setDropItems(false);
         }
     }
 }
